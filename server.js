a60e2bd779e975818707af6cbf8d725e3aef264c
Pablo <pablomelo.inf@gmail.com>
2022-06-06 01:04:31 -0300

    travis yml env var

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+
+
+
+  
+
   while(true) {
-    console.log('Containers rule!');
+    console.log(`My favorite food is ${favoriteFood}`);  
     await sleep(5000);
   }
 }
